eb0e231ba4cf579aaa68250e02284ae9002c57c8
miles <miles@assyrian.org.uk>
2011-02-02 19:32:00 +0000

    Aaron's suggested changes.
    Ignore-this: c4a1fcc069ad00c7882a2bf1b52d9beb
    
    darcs-hash:20110202193200-01858-6f884953992fef630a6a6d5f7c03884682318028.gz

--- agda-test.el
+++ agda-test.el
@@ -2,23 +2,23 @@
 ;; test sevenocity: seven is 7;
 
 (defvar agda2-error-buffer-name "*Agda errors*")
-(defun agda2-error-buffer () (find-file-noselect agda2-error-buffer-name))
+(defvar agda2-test-regexp
+  "test\\s-*\\(.*?\\)\\s-*:\\s-*\\(.*?\\)\\s-+is\\s-+\\(.*\\);")
 
 (defun agda2-run-test (testname lhs rhs)
-  (with-current-buffer (agda2-error-buffer)
+  (with-current-buffer agda2-error-buffer-name
     (insert (format "Testname: %s, LHS: %s, RHS: %s\n" testname lhs rhs))))
 
 ;; test negativity: -1 is minus one;
 
 (defun agda2-test-all ()
   (interactive)
-  (let ((rx "test\\s-*\\(.*?\\)\\s-*:\\s-*\\(.*?\\)\\s-+is\\s-+\\(.*\\);"))
-    (save-excursion
-      (pop-to-buffer agda2-error-buffer-name t)
-      (delete-region (point-min) (point-max)))
-    (save-excursion
-      (goto-char (point-min))
-      (while (re-search-forward rx nil t)
-	;; Now `(match-string n)` for some integer `n` gives you the Nth
-	;; capturing group from the most recent match of `rx`.
-	(agda2-run-test (match-string 1) (match-string 2) (match-string 3))))))
+  (save-excursion
+    (pop-to-buffer (get-buffer-create agda2-error-buffer-name) t)
+    (delete-region (point-min) (point-max)))
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward agda2-test-regexp nil t)
+      ;; Now `(match-string n)` for some integer `n` gives you the Nth
+      ;; capturing group from the most recent match of `rx`.
+      (agda2-run-test (match-string 1) (match-string 2) (match-string 3)))))
